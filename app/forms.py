c3354782e93e87e8d19a58984bb65167bc77f174
AnOctopus <sean.andrew.walker@gmail.com>
2015-04-06 10:56:17 -0700

    remove reduntant tripcode forms

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
@@ -7,14 +7,12 @@ class PostForm(Form):
 	title = StringField('title', validators=[DataRequired()])
 	email = StringField('email')
 	name = StringField('name')
-	tripcode = StringField('trip')
-	body = StringField('body', validators=[DataRequired()])
+	body = TextAreaField('body', validators=[DataRequired()])
 	file = FileField('file', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
 	
 class ReplyForm(Form):
 	title = StringField('title')
 	email = StringField('email')
 	name = StringField('name')
-	tripcode = StringField('trip')
-	body = StringField('body', validators=[DataRequired()])
+	body = TextAreaField('body', validators=[DataRequired()])
 	file = FileField('file', validators=[FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
